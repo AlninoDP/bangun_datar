33b87391080d1007b8d845d64d2612d7a1e87bab
Laptop Alnino <alnino007@gmail.com>
2024-04-26 15:18:24 +0700

    tambaha onChanged method untuk custom_textfield

--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({
-    super.key,
-    required this.unitText,
-    required this.hintText,
-    this.textController,
-  });
+  const CustomTextField(
+      {super.key,
+      required this.unitText,
+      required this.hintText,
+      required this.textController,
+      this.onChanged});
   final String hintText;
   final String unitText;
-  final TextEditingController? textController;
+  final TextEditingController textController;
+  final ValueChanged? onChanged;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -26,6 +27,7 @@ class CustomTextField extends StatelessWidget {
               border: Border.all(),
             ),
             child: TextField(
+              onChanged: onChanged,
               controller: textController,
               keyboardType:
                   const TextInputType.numberWithOptions(decimal: true),
