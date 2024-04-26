5d79214f96e5599c5268e3ff78dce443085c0947
Laptop Alnino <alnino007@gmail.com>
2024-04-26 15:20:25 +0700

    tambah dokumentasi method dan tambah menu belah ketupat ke switch case

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shapes_formula/constant/constant.dart';
+import 'package:shapes_formula/pages/page_belah_ketupat.dart';
 import 'package:shapes_formula/pages/page_lingkaran.dart';
-import 'package:shapes_formula/widgets/custom_alert_dialog.dart';
 import 'package:shapes_formula/widgets/dropdown_shape.dart';
 
 class HomePage extends StatefulWidget {
@@ -38,6 +38,7 @@ class _HomePageState extends State<HomePage> {
             const SizedBox(
               height: 20,
             ),
+            // Dropdown
             Center(
               child: DropdownShape(
                 selectedShape: _selectedShape,
@@ -45,9 +46,16 @@ class _HomePageState extends State<HomePage> {
                     _onDropDownChanged(newSelectedShape),
               ),
             ),
-            _selectedShape != null
-                ? _shapesFormulaMenu(_selectedShape, context)
-                : const Placeholder()
+
+            const SizedBox(height: 10),
+
+            // Content Body
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 1000),
+              child: _selectedShape != null
+                  ? _shapesFormulaMenu(_selectedShape)
+                  : const SizedBox(),
+            ),
           ],
         ),
       )),
@@ -55,12 +63,13 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-Widget _shapesFormulaMenu(BangunDatar? bangunDatar, BuildContext context) {
+/// Untuk menampilkan menu berdasarkan bangun datar
+Widget _shapesFormulaMenu(BangunDatar? bangunDatar) {
   switch (bangunDatar) {
     case BangunDatar.lingkaran:
       return PageLingkaran(imagePath: _getShapesImagePath(bangunDatar));
     case BangunDatar.belahKetupat:
-      return Placeholder();
+      return PageBelahKetupat(imagePath: _getShapesImagePath(bangunDatar));
     case BangunDatar.layangLayang:
       return Placeholder();
     case BangunDatar.trapesium:
@@ -75,7 +84,7 @@ Widget _shapesFormulaMenu(BangunDatar? bangunDatar, BuildContext context) {
   }
 }
 
-/// to set image asset path based on dropdown value
+/// Untuk set imagepath berdasarkan bangun datarnya
 String _getShapesImagePath(BangunDatar? bangunDatar) {
   const basePath = 'assets/images';
   switch (bangunDatar) {
@@ -95,11 +104,4 @@ String _getShapesImagePath(BangunDatar? bangunDatar) {
     default:
       return '';
   }
-}
-
-void _showAnswer(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (context) => CustomAlertDialog(contentText: 'Test'),
-  );
 }
